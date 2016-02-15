a5b7aa021333ce61fdf9f4883d9bf2e1280fb0ad
Mars <marshallmiller92@gmail.com>
2016-02-15 14:34:21 -0800

    working on connection data correctly

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 var timeArray = ['8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ']
 
+CampCoffee.mathCustomer();
+CampCoffee.mathCups();
+CampCoffee.mathPounds();
+
 var locationData = [new CampCoffee('Pike Place Market', 14, 55, 1.2, 3.7),
 new CampCoffee('Capitol Hill', 32, 48, 3.2, 0.4),
 new CampCoffee('Seattle Public Library', 49, 75, 2.6, 0.2),
@@ -32,28 +36,52 @@ CampCoffee.prototype.mathPounds = function() {
 
 CampCoffee.prototype.mathCups = function() {
   for (var i = 0; i < timeArray.length; i++) {
-    this.cupsHour.push((this.cupsHour[i] * this.cups) / 20);
+    this.cupsHour.push((this.customerHour[i] * this.cups));
   }
 }
 
 CampCoffee.prototype.renderCustomers = function() {
+  var sectionEl = document.getElementById('sectionCustomers');
+  var tableEl = document.createElement('table');
+  var rowEl = document.createElement('tr');
 
+  var headEl = document.createElement('th');
+    headEl.textContent = this.location;
+    rowEl.appendChild(headEl);
+
+  for (var i = 0; i < timeArray.length; i++) {
+    var dataEl = document.createElement('td');
+    dataEl.textContent = this.customerHour[i];
+    rowEl.appendChild(dataEl);
+  }
+
+  tableEl.appendChild(rowEl);
+  sectionEl.appendChild(tableEl);
 }
 
 CampCoffee.prototype.renderCups = function() {
+  var sectionEl = document.getElementById('sectionCups');
+  var tableEl = document.createElement('table');
+  var rowEl = document.createElement('tr');
 
+  var headEl = document.createElement('th');
+    headEl.textContent = this.location;
+    rowEl.appendChild(headEl);
+
+  for (var i = 0; i < timeArray.length; i++) {
+    var dataEl = document.createElement('td');
+      dataEl.textContent = this.cupsHour[i].toFixed(1);
+      rowEl.appendChild(dataEl);
+  }
+
+  tableEl.appendChild(rowEl);
+  sectionEl.appendChild(tableEl);
 }
 
 CampCoffee.prototype.renderPounds = function() {
-
-  this.mathCustomer();
-  this.mathCups();
-  this.mathPounds();
-
-  var sectionEl = document.getElementById('sectionTable');
+  var sectionEl = document.getElementById('sectionPounds');
   var tableEl = document.createElement('table');
   var rowEl = document.createElement('tr');
-  var rowEl2 = document.createElement('tr');
 
   var headEl = document.createElement('th');
     headEl.textContent = this.location;
@@ -90,5 +118,7 @@ function tableDisplay() {
 
 tableDisplay();
 for (var i = 0; i < locationData.length; i++){
-  locationData[i].render();
+  locationData[i].renderPounds();
+  locationData[i].renderCups();
+  locationData[i].renderCustomers();
 };
